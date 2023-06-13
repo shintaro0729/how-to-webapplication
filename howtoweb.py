4b0045c7c0500abb8532177205af6be18575b182
shintaro0729 <shintaro20010729iroiro@gmail.com>
2023-06-13 12:15:03 +0900

    再コミット

--- howtoweb.py
+++ howtoweb.py
@@ -16,19 +16,19 @@ st.write("webアプリの公開")
 """
 # webアプリの公開手順
 
-## githubを開く
-### リポジトリを作成
+## 1. githubを開く
+### 1.1 リポジトリを作成
 Create a new repositoryから作成
 Repository nameを入力
 Public or Privateの選択
 Create repositoryを選択
 HTTPSのURLをコピーする
-## ターミナルを開く
-### gitの初期化
+## 2. ターミナルを開く
+### 2.1 gitの初期化
 ```python
 git init
 ```
-### ローカルとリポジトリを紐づける
+### 2.2 ローカルとリポジトリを紐づける
 ```python
 git remote add origin urlをペースト
 ```
@@ -36,7 +36,7 @@ git remote add origin urlをペースト
 ```python
 git remote rm origin
 ```
-### requirements.tetの用意
+### 2.3 requirements.tetの用意
 requirements.tetに、アプリケーションで使用される外部ライブラリを記載する
 #### ライブラリのバージョンを確認
 streamlitのバージョンを確認する場合、以下のように実行する
@@ -49,7 +49,7 @@ streamlitの場合、以下のように入力する
 ```python
 streamlit==バージョン名
 ```
-### gitに追加する
+### 2.4 gitに追加する
 ```python
 git add .
 git commit -m'1stcommit
@@ -58,8 +58,8 @@ git push origin master
 これでgitにプッシュされる
 以上でリポジトリの作成が完了
 
-## Streamlit sharingを開く
-### リポジトリの追加
+## 3. Streamlit sharingを開く
+### 3.1 リポジトリの追加
 New appを選択
 リポジトリからgitで作成したリポジトリを選択
 Branchにmasterからmainを入力
